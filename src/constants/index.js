eb9c3e0cf6da5f3c48bb790556c3a077fa578de1
OumarouSandaSouley <oumarousandasouley>
2024-11-17 00:05:56 +0100

    changes in testimonials

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,12 +6,12 @@ import {
   PlugZap,
   GlobeLock,
 } from "lucide-react";
-import user1 from "../assets/users/user1.jpg";
-import user2 from "../assets/users/user2.jpg";
-import user3 from "../assets/users/user3.jpg";
-import user4 from "../assets/users/user4.jpg";
-import user5 from "../assets/users/user5.jpg";
-import user6 from "../assets/users/user6.jpg";
+import user1 from "../assets/profile-pictures/user1.jpg";
+import user2 from "../assets/profile-pictures/user2.jpg";
+import user3 from "../assets/profile-pictures/user3.jpg";
+import user4 from "../assets/profile-pictures/user4.jpg";
+import user5 from "../assets/profile-pictures/user5.jpg";
+import user6 from "../assets/profile-pictures/user6.jpg";
 
 export const navItems = [
   { label: "Features", href: "#Features" },
